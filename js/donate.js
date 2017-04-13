3261d057d64c46cbe4a528b7c41fcc7b7642a953
abhishek1995s <abhisheks6477@gmail.com>
2017-04-13 20:36:13 +0530

    initial commit
    implemented donatio  using paypal and stripe

--- js/donate.js
+++ js/donate.js
@@ -1,3 +1,5 @@
+/* function to show and hide donation and regular supporter part of paypal and stripe donation.
+This make use of hide and appear css classes in css/custom.css file*/
 
   function hider(channel,no) {
   
@@ -55,5 +57,63 @@
 
 }}
 
+/* For stripe payment.
+Two seperate jquery functions are used in stripe payment for donation and regular supporter.
+On clicking the donate button with ids customButton and customButtone respective fumctions 
+will be called*/
+  
+  
+  var handler = StripeCheckout.configure({
+    key: 'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
+    image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+    token: function (token) {
+        $("#stripeToken").val(token.id);
+        $("#stripeEmail").val(token.email);
+        $("#amount").val($("#amount").val() * 100);
+        $("#myForm").submit();
+    }
+});
 
-   
+$('#customButton').on('click', function (e) {
+    var amount = $("#amount").val() * 100;
+    var displayAmount = parseFloat(Math.floor($("#amount").val() * 100) / 100).toFixed(2);
+    // Open Checkout with further options
+    handler.open({
+        name: 'Demo Site',
+        description: 'Custom amount ($' + displayAmount + ')',
+        amount: amount
+    });
+    e.preventDefault();
+});
+
+// Close Checkout on page navigation
+$(window).on('popstate', function () {
+    handler.close();
+});
+var handler = StripeCheckout.configure({
+    key: 'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
+    image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+    token: function (token) {
+        $("#stripeTokene").val(token.id);
+        $("#stripeEmaile").val(token.email);
+        $("#amounte").val($("#amount").val() * 100);
+        $("#myForme").submit();
+    }
+});
+
+$('#customButtone').on('click', function (e) {
+    var amount = $("#amounte").val() * 100;
+    var displayAmount = parseFloat(Math.floor($("#amounte").val() * 100) / 100).toFixed(2);
+    // Open Checkout with further options
+    handler.open({
+        name: 'Demo Site',
+        description: 'Custom amount ($' + displayAmount + ')',
+        amount: amount
+    });
+    e.preventDefault();
+});
+
+// Close Checkout on page navigation
+$(window).on('popstate', function () {
+    handler.close();
+});
