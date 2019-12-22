8be89c17c9b5e310c07629408a61303525759b42
STEdirisuriya <supunedirisuriya2002@gmail.com>
2019-12-22 11:01:40 +0530

    added back to top button

--- js/scripts.js
+++ js/scripts.js
@@ -363,3 +363,21 @@ $(window).load(function() {
             return false;
         }   
 });
+
+//Back to top button
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
