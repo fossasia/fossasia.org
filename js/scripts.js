1d1481365828c2a327793af52c8c79a2c3883d80
Rohit Singh Rathaur <rohitrathore.imh55@gmail.com>
2020-01-18 12:05:38 +0530

    smooth

--- js/scripts.js
+++ js/scripts.js
@@ -368,7 +368,7 @@ $(window).load(function() {
 <script>
 $(document).ready(function(){
   // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+  $(".myBtn").on('click', function(event) {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
