40c457a8ccd80792884c5bf88fa7f085e0fae745
Rohit Singh Rathaur <rohitrathore.imh55@gmail.com>
2020-01-18 11:30:42 +0530

    smooth scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -365,20 +365,27 @@ $(window).load(function() {
 });
 
 //Back to top button
-jQuery(document).ready(function() {
-    var offset = 220;
-    var duration = 500;
-    jQuery(window).scroll(function() {
-        if (jQuery(this).scrollTop() > offset) {
-            jQuery('.myBtn').fadeIn(duration);
-        } else {
-            jQuery('.myBtn').fadeOut(duration);
-        }
-    });
-
-    jQuery('.myBtn').click(function(event) {
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
         event.preventDefault();
-        jQuery('html, body').animate({scrollTop: 0}, duration);
-        return false;
-    })
-}); 
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+  
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
