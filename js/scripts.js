2ed7f257b390be2ccc8f8d2cc500cbc9b825bf90
Rohit Singh Rathaur <rohitrathore.imh55@gmail.com>
2020-01-18 11:13:28 +0530

    added smooth scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -365,8 +365,6 @@ $(window).load(function() {
 });
 
 //Back to top button
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script>
 $(document).ready(function(){
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
@@ -391,4 +389,3 @@ $(document).ready(function(){
     } // End if
   });
 });
-</script>
