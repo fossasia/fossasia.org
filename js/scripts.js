2ffb9928e19e65f92df68543b90cefb2672feffd
Rohit Singh Rathaur <rohitrathore.imh55@gmail.com>
2020-01-18 11:04:03 +0530

    Added smooth scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -363,3 +363,32 @@ $(window).load(function() {
             return false;
         }   
 });
+
+//Back to top button
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+</script>
